9623386c8aa2152236cc587e07ae707e1cb838ac
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-09-04 22:30:19 -0300

    fetching api,filter data list,push forecast date,added icon png,description of the weather

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var inputElement = document.getElementById("inputCity");
 // console.log(inputElment);
 var btnElement = document.getElementById("btn-City");
 // console.log(btnElement);
-var weatherCards = document.getElementsByClassName("weatherFiveDays");
+const weatherCards = document.querySelector(".weatherFiveDays");
 // console.log("check",weatherCards);
-
+const weatherTodayDiv =document.querySelector(".weatherToday .details");
+// console.log(weatherTodayDiv);
 
 const ApiKey = 'dcf43dd6744f3456f9b93aca898f1db6'; //weather APi key
 
@@ -19,45 +20,86 @@ const cardWeather = (itemWeather) => {
 
 function GeoCoordinate(event) {
     event.preventDefault();
-var cityName =inputElement.value;
+var cityName =inputElement.value.trim();
 // console.log(input_check);
-if (!cityName){
-    console.error("please enter city name");
+
+//check user input
+if (!cityName ){
+    alert("please enter city name");
+} else if(!isNaN(cityName)){
+    alert("city cannot be a number");
     return;
 }
 
-// Get weather details like rain, humidity and temperature, date etc
-const getweatherdetails = (cityName,lat,lon) => {
-    const weather_api =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`;
-
-    fetch (weather_api).then (res => res.json()).then(data => {
-        console.log('weather',data);
-        const forecastDays =[];
-        const fiveDaysForecast=data.list.filter(forecast =>{
-            const forecastDate = new Date(forecast.dt_txt).getDate();
-            if(!forecastDays.includes(forecastDate)) {
-               return forecastDays.push(forecastDate);
-            }
-        });
-        console.log(fiveDaysForecast);
-        fiveDaysForecast.forEach(itemWeather => { 
-          weatherCards.insertAdjacentHTML("beforeend", cardWeather(itemWeather));
-        
-        });
-    }) .catch (() =>{
-        alert("Error fetching the weather  forecast data");
-    });
-
-    }
-
 const Geocoding_link = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${ApiKey}`;
 //Get entered city coordinates from the api
-fetch (Geocoding_link).then (res => res.json()).then(data => {
+fetch (Geocoding_link)
+.then((res) => {
+    if (!res.ok) {
+        return res.json().then((body)=> {
+            console.log('geocoding error:',body);
+
+            throw new Error('Geocoding API error');
+        });
+    }
+    return res.json();
+})
+.then(data => {
+ 
     const {name ,lat, lon} = data[0];
-    getweatherdetails(name, lat, lon);
-}).catch(() => {
-    alert("Error fetching to show cooordinates");
+    getweatherdetails (name, lat, lon);
+})
+.catch((err) => {
+    alert ('Error fetching to show coordinates');
 });
-}
+
+
+const getweatherdetails = (cityName, lat, lon) =>{
+    const weather_api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`;
+    
+    //fetching weather details
+    fetch (weather_api)
+    .then((res) => {
+//check if response is successful
+if (!res.ok) {
+    throw new Error('Failed to fetch weather details');
+    }
+    return res.json();
+    })
+    .then((data) => {
+        // console.log (data);
+        console.log (data.list);
+        const forecastDays =[];
+        const fiveDaysForecast = data.list.filter((forecast) => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+
+            //Get 5 days forecast and check if the forecast date is not already added to the forecastDays array
+            if (forecastDate.length<5 && !forecastDays.includes(forecastDate)) {
+                //add forecast date to forecastDays DOM
+                forecastDays.push(forecastDate);
+                return true;
+            }
+        });
+        //Get weather details like rain, humidity ,temperature, date etc
+        // today weather
+        const todayWeather = data.list[0];
+        //To display weather icons for today weather
+        const iconUrl = `https://openweathermap.org/img/w/${todayWeather.weather[0].icon}.png`;
+        //Accessing weather description 
+        const weatherDescription = todayWeather.weather[0].description;
+        
+
+    });
+
+}};
+
+
+
+
+        
+
+
+
+
 btnElement.addEventListener("click", GeoCoordinate);
 
